c2c7802668072f4a0f0d36df3fe3e7e68e475686
PrakashPalani <prakashpalani009@gmail.com>
2026-01-04 01:07:39 +0530

    added Myapplications to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Jobs from "./pages/Jobs";
-import Navbar from "./components/Navbar";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
+
+import Login from "./pages/Login";
+import Jobs from "./pages/Jobs";
+import MyApplications from "./pages/MyApplications";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   const { user } = useContext(AuthContext);
@@ -17,7 +19,7 @@ export default function App() {
           path="/"
           element={user ? <Jobs /> : <Navigate to="/login" />}
         />
-        <Route path="/applications" element={<MyApplications />} />
+        <Route path="/applications" element={user ? <MyApplications /> : <Navigate to="/login" />} />
       </Routes>
     </>
   );
